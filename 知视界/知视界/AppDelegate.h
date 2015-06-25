66cd40c1422f45a38b4a113d0d38f8aec6ec1b66
gaoqing1993 <794343608@qq.com>
2015-06-25 22:15:14 +0800

    新建工程

---
+++ 知视界/知视界/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  知视界
+//
+//  Created by lanou3g on 15/6/25.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 知视界/知视界/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  知视界
+//
+//  Created by lanou3g on 15/6/25.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
